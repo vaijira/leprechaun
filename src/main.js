a703abc6b77750a8e42c3c132e26454f2f29f7bf
Jorge Perez Burgos <vaijira@gmail.com>
2024-03-17 21:35:06 +0100

    First functional version.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
-// import { PDFDocument } from 'https://unpkg.com/pdf-lib@1.7.0/dist/pdf-lib.min.js';
 import { PDFDocument } from 'pdf-lib';
+import downloadjs from 'downloadjs';
 
-async function fillForm() {
+const COUNTRY = 'SPAIN';
+const BROKER = 'Interactive Brokers Ireland Limited';
+
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+function getStrDate() {
+  const d = new Date();
+  return d.toLocaleString('en-us', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  }).replace(/(\d+)\/(\d+)\/(\d+)/, '$2-$1-$3').replaceAll('-','');
+}
+
+async function fillForm(name, address, dni) {
   const formUrl = 'assets/files/form-8-3-6-interest-withholding-tax.pdf';
   const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer());
 
@@ -9,16 +32,16 @@ async function fillForm() {
 
   const form = pdfDoc.getForm();
   const nameField = form.getTextField('Name of person');
-  nameField.setText('John Smith');
+  nameField.setText(name);
 
   const addressField = form.getTextField('Address of person');
-  addressField.setText('Revenue street');
+  addressField.setText(address);
 
   const dniField = form.getTextField('Tax reference number in country of residence where relevant');
-  dniField.setText('XXXXX');
+  dniField.setText(dni);
 
   const taxResidentCountryField = form.getTextField('tax resident in');
-  taxResidentCountryField.setText('SPAIN');
+  taxResidentCountryField.setText(COUNTRY);
 
   const taxNumberField = form.getTextField('tax at a rate not to exceed');
   taxNumberField.setText('0');
@@ -27,21 +50,36 @@ async function fillForm() {
   certificateOfResidenceField.check();
 
   const doubleTaxationCountryField = form.getTextField('B  I confirm that the Interest Article of Irelands Double Taxation Agreement with');
-  doubleTaxationCountryField.setText('SPAIN');
+  doubleTaxationCountryField.setText(COUNTRY);
 
   const personInterestField = form.getTextField('insert name of person making the interest');
-  personInterestField.setText('Interactive Brokers Ireland Limited');
+  personInterestField.setText(BROKER);
 
   const writtenNoticeField = form.getTextField('written notice');
-  writtenNoticeField.setText('Interactive Brokers Ireland Limited');
+  writtenNoticeField.setText(BROKER);
 
   const dateField = form.getTextField('Text1');
-  dateField.setText('17032024');
+  dateField.setText(getStrDate());
 
-  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
-  document.getElementById('pdf').src = pdfDataUri;
+  return await pdfDoc.save();
 }
 
-export function main() {
-  fillForm();
+export async function merge(name, address, dni, residenceFile) {
+  const formBytes = await fillForm(name, address, dni);
+  const formPdf = await PDFDocument.load(formBytes);
+
+  const aeatBytes = await readFileAsync(residenceFile);
+  const aeatPdf = await PDFDocument.load(aeatBytes);
+
+  const mergePdf = await PDFDocument.create();
+
+  const copiedPagesA = await mergePdf.copyPages(formPdf, formPdf.getPageIndices());
+  copiedPagesA.forEach((page) => mergePdf.addPage(page));
+
+  const copiedPagesB = await mergePdf.copyPages(aeatPdf, aeatPdf.getPageIndices());
+  copiedPagesB.forEach((page) => mergePdf.addPage(page));
+
+  const mergedPdfFile = await mergePdf.save();
+
+  downloadjs(mergedPdfFile, 'form-8-3-6.pdf', 'application/pdf');
 }
